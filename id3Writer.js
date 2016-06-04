1b45ee327f0510007ba7c084d994bc6477d32d84
Guillaume Bouffard <guillaume@getkahoot.com>
2016-06-04 10:40:17 +0100

    id3Writer v1.0.0

--- id3Writer.js
+++ id3Writer.js
@@ -1,1 +1,46 @@
-console.log('yo');
+var recursive = require("recursive-readdir-synchronous");
+var nodeID3 = require("node-id3");
+var fs = require("fs");
+var request = require("request");
+var getJSON = require("get-json");
+
+var files = recursive("./Files", [".DS_Store"]);
+var i;
+
+var download = function(uri, filename){
+  "use strict";
+  request.head(uri, function(error, res, body){
+    request(uri).pipe(fs.createWriteStream(filename));
+  });
+};
+
+for (i = 0; i < files.length; i++) {
+  var artistTitleArray = files[i].slice(6).replace(/.mp3/, "").split(" - ");
+  var artist = artistTitleArray[0];
+  var title = artistTitleArray[1];
+
+  var albumUrl = (
+    "https://api.spotify.com/v1/search?q=artist:"
+    + artist.replace(/ /g, "%20")
+    + "%20track:"
+    + title.replace(/ /g, "%20")
+    + "&type=track"
+  );
+
+  getJSON(albumUrl, function(err, response) {
+    var coverUrl = response.tracks.items[0].album.images[0].url;
+    download(coverUrl, "albumCover.jpeg");
+  });
+
+  var coverImage = "./albumCover.jpeg";
+  var tags = {
+    artist: artist,
+    title: title,
+    album: "",
+    composer: "",
+    image: coverImage
+  };
+
+  var success = nodeID3.write(tags, files[i]);
+  console.log(success? (artist +' - ' + title) : "failed: " + (artist + ' - ' + title));
+}
