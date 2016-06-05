2299884cc75ca1e00412dbaf94f8063f8f49ecde
Guillaume Bouffard <guillaume@getkahoot.com>
2016-06-05 09:10:35 +0100

    Use of an immediately-invoked function expression to create a closure around setTimeout - gets new cover every 5 seconds for now

--- id3Writer.js
+++ id3Writer.js
@@ -5,7 +5,6 @@ var request = require("request");
 var getJSON = require("get-json");
 
 var files = recursive("./Files", [".DS_Store"]);
-var i;
 
 var download = function(uri, filename){
   "use strict";
@@ -14,33 +13,38 @@ var download = function(uri, filename){
   });
 };
 
-for (i = 0; i < files.length; i++) {
-  var artistTitleArray = files[i].slice(6).replace(/.mp3/, "").split(" - ");
-  var artist = artistTitleArray[0];
-  var title = artistTitleArray[1];
+for (var i = 1; i < files.length; i++) {
+  (function(index) {
+    var artistTitleArray = files[i].slice(6).replace(/.mp3/, "").split(" - ");
+    var artist = artistTitleArray[0];
+    var title = artistTitleArray[1];
 
-  var albumUrl = (
-    "https://api.spotify.com/v1/search?q=artist:"
-    + artist.replace(/ /g, "%20")
-    + "%20track:"
-    + title.replace(/ /g, "%20")
-    + "&type=track"
-  );
+    var spotifyAPI = (
+      "https://api.spotify.com/v1/search?q=artist:"
+      + artist.replace(/ /g, "%20")
+      + "%20track:"
+      + title.replace(/ /g, "%20")
+      + "&type=track"
+    );
 
-  getJSON(albumUrl, function(err, response) {
-    var coverUrl = response.tracks.items[0].album.images[0].url;
-    download(coverUrl, "albumCover.jpeg");
-  });
+    setTimeout(function() {
+      getJSON(spotifyAPI, function(err, response) {
+        var coverUrl = response.tracks.items[0].album.images[0].url;
+        download(coverUrl, "albumCover.jpeg");
+      });
 
-  var coverImage = "./albumCover.jpeg";
-  var tags = {
-    artist: artist,
-    title: title,
-    album: "",
-    composer: "",
-    image: coverImage
-  };
+      // var coverImage = "./albumCover.jpeg";
+      // var tags = {
+      //   artist: artist,
+      //   title: title,
+      //   album: "",
+      //   composer: "",
+      //   image: coverImage
+      // };
 
-  var success = nodeID3.write(tags, files[i]);
-  console.log(success? (artist +' - ' + title) : "failed: " + (artist + ' - ' + title));
+      // var success = nodeID3.write(tags, files[i]);
+      // console.log(success? (artist +' - ' + title) : "failed: " + (artist + ' - ' + title));
+
+    }, i * 5000);
+  })(i);
 }
