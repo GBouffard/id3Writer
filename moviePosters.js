692fc518bdb6be039c4ddea0543a0f84771ce32c
Guillaume Bouffard <guillaume@getkahoot.com>
2016-06-11 19:03:53 +0100

    Add moviePosters.js

---
+++ moviePosters.js
@@ -0,0 +1,31 @@
+var recursive = require("recursive-readdir-synchronous");
+var fs = require("fs");
+var request = require("request");
+var getJSON = require("get-json");
+
+var files = recursive("./Files", [".DS_Store"]);
+
+var download = function(uri, filename){
+  "use strict";
+  request.head(uri, function(error, res, body){
+    request(uri).pipe(fs.createWriteStream(filename));
+  });
+};
+
+for (var i = 0; i < files.length; i++) {
+  (function(index) {
+    var movieTitle = files[i].slice(6).replace(/.avi|.wmv|.mpeg|.mp4/, "");
+
+    var imdbAPI = (
+      "http://www.omdbapi.com/?y=&plot=short&r=json&t="
+      + movieTitle.replace(/ /g, "%20").replace(/'/g, "%27")
+    );
+
+    setTimeout(function() {
+      getJSON(imdbAPI, function(err, response) {
+        var coverUrl = response.Poster;
+        download(coverUrl, "./Files/" + movieTitle + ".jpeg");
+      });
+    }, i * 4000);
+  })(i);
+}
