143660722f3d230c875780af203602d4da6b3d05
Guillaume Bouffard <guillaume@getkahoot.com>
2017-06-23 10:47:15 +0100

    Add copy of spotify_tokens

---
+++ spotify_tokens_copy.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var spotifyTokens = {
+  clientID: function() {
+    return "my spotify token number";
+  },
+  clientSecret: function() {
+    return "my spotify secret number";
+  }
+};
+
+module.exports = spotifyTokens;
